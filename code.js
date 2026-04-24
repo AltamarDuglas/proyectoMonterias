5e0b3064f9a8ebffbb6b7bf80f82b4ccd40058af
AltamarDuglas <duglasphone@gmail.com>
2026-04-23 21:18:53 -0500

    Correccion en el modal filtros

--- code.js
+++ code.js
@@ -1,8 +1,3 @@
-// ============================================================================
-// ARCHIVO DE SCRIPTS (JavaScript)
-// Contiene la lógica de interactividad del lado del cliente.
-// Se aplican principios de claridad y responsabilidad para funciones pequeñas.
-// ============================================================================
 
 // Seleccionamos el contenedor del menú desplegable de categorías.
 // Se utiliza 'let' porque en el futuro podríamos necesitar reasignar la variable, 
@@ -25,7 +20,7 @@ function mostrarCategorias() {
 }
 
 // ----------------------------------------------------------------------------
-// LÓGICA PARA EL MENÚ HAMBURGUESA (Mobile First)
+// LÓGICA PARA EL MENÚ HAMBURGUESA 
 // ----------------------------------------------------------------------------
 
 // Seleccionamos el botón de menú hamburguesa usando su ID
@@ -39,10 +34,26 @@ const navbarMenu = document.getElementById('navbarMenu');
  * Cuando el usuario hace 'click', se ejecuta la función de callback.
  * Esta lógica es esencial para la responsividad en dispositivos móviles.
  */
-if(menuToggle && navbarMenu) {
+if (menuToggle && navbarMenu) {
     menuToggle.addEventListener('click', () => {
         // La función toggle() añade la clase 'active' si no existe, o la quita si existe.
         // Esto permite abrir y cerrar el menú móvil con un solo botón.
         navbarMenu.classList.toggle('active');
     });
 }
+
+
+let btnfiltro = document.getElementById("btn-filtros")
+let filtros = document.querySelector('.filtros')
+
+
+    btnfiltro.addEventListener('click', () => {
+        if (filtros.classList.contains("active")) {
+            
+            filtros.classList.remove('active')
+            
+        } else {
+            filtros.classList.add('active')
+            console.log(filtros)
+        }
+    })
